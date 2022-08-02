08ba503b493aa87710a7fff4941c7f82523a02a6
lingdongyong <1146539132@qq.com>
2022-08-02 11:35:49 +0800

    feat：stream

--- stream_grpc_test/client/client.go
+++ stream_grpc_test/client/client.go
@@ -1,1 +1,63 @@
-package client
+package main
+
+import (
+	v1 "GoStudy/stream_grpc_test/proto/common/stream/proto/v1"
+	"context"
+	"fmt"
+	"google.golang.org/grpc"
+	"sync"
+	"time"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost:50052", grpc.WithInsecure())
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+	c := v1.NewGreeterClient(conn)
+
+	//res, _ := c.GetStream(context.Background(), &v1.StreamReqData{Data: "慕课网"})
+	//for {
+	//	a, err := res.Recv()
+	//	if err != nil {
+	//		fmt.Println(err)
+	//		break
+	//	}
+	//	fmt.Println(a.Data)
+	//}
+	//
+	//putS, _ := c.PutStream(context.Background())
+	//i := 0
+	//for {
+	//	i++
+	//	putS.Send(&v1.StreamReqData{
+	//		Data: fmt.Sprintf("啦啦啦%d", i),
+	//	})
+	//	time.Sleep(time.Second)
+	//	if i > 10 {
+	//		break
+	//	}
+	//}
+
+	allStr, _ := c.AllStream(context.Background())
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		for {
+			a, _ := allStr.Recv()
+			fmt.Println("收到服务端消息" + a.Data)
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		for {
+			_ = allStr.Send(&v1.StreamReqData{Data: "我是客户端"})
+			time.Sleep(time.Second)
+		}
+	}()
+
+	wg.Wait()
+}
--- stream_grpc_test/server/server.go
+++ stream_grpc_test/server/server.go
@@ -1,1 +1,76 @@
-package server
+package main
+
+import (
+	v1 "GoStudy/stream_grpc_test/proto/common/stream/proto/v1"
+	"fmt"
+	"google.golang.org/grpc"
+	"net"
+	"sync"
+	"time"
+)
+
+type server struct {
+	v1.UnimplementedGreeterServer
+}
+
+func (s *server) GetStream(req *v1.StreamReqData, res v1.Greeter_GetStreamServer) error {
+	i := 0
+	for {
+		i++
+		res.Send(&v1.StreamResData{
+			Data: fmt.Sprint("%v", time.Now().Unix()),
+		})
+		time.Sleep(time.Second)
+		if i > 10 {
+			break
+		}
+	}
+	return nil
+}
+
+func (s *server) PutStream(cliStr v1.Greeter_PutStreamServer) error {
+	for {
+		if a, err := cliStr.Recv(); err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Println(a.Data)
+		}
+	}
+	return nil
+}
+
+func (s *server) AllStream(allStr v1.Greeter_AllStreamServer) error {
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		for {
+			a, _ := allStr.Recv()
+			fmt.Println("收到客户端消息" + a.Data)
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		for {
+			_ = allStr.Send(&v1.StreamResData{Data: "我是服务端"})
+			time.Sleep(time.Second)
+		}
+	}()
+
+	wg.Wait()
+	return nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", "localhost:50052")
+	if err != nil {
+		panic(err)
+	}
+	s := grpc.NewServer()
+	v1.RegisterGreeterServer(s, &server{})
+	err = s.Serve(lis)
+	if err != nil {
+		panic(err)
+	}
+}
